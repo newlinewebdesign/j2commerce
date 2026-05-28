ecc8a5e1250962d31717ef627bf411bed01c39bc
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2026-05-27 20:54:27 -0400

    Avoid drop of index (#1107)

--- administrator/components/com_j2commerce/sql/updates/mysql/6.2.2-2026-04-17.sql
+++ administrator/components/com_j2commerce/sql/updates/mysql/6.2.2-2026-04-17.sql
@@ -2,5 +2,6 @@ UPDATE `#__j2commerce_paymentprofiles`
     SET `payment_token` = ''
     WHERE `payment_token` IS NULL;
 
+ALTER TABLE `#__j2commerce_paymentprofiles` ROW_FORMAT=DYNAMIC /** CAN FAIL **/;
 ALTER TABLE `#__j2commerce_paymentprofiles`
-    MODIFY `payment_token` VARCHAR(100) NOT NULL DEFAULT '' /** CAN FAIL **/;
+    MODIFY `payment_token` VARCHAR(255) NOT NULL DEFAULT '';
--- administrator/components/com_j2commerce/sql/updates/mysql/6.3.2-2026-05-27.sql
+++ administrator/components/com_j2commerce/sql/updates/mysql/6.3.2-2026-05-27.sql
@@ -3,9 +3,6 @@ UPDATE `#__j2commerce_paymentprofiles`
     SET `payment_token` = ''
     WHERE `payment_token` IS NULL;
 
-ALTER TABLE `#__j2commerce_paymentprofiles`
-    DROP INDEX `uq_user_provider_env_token` /** CAN FAIL **/;
+ALTER TABLE `#__j2commerce_paymentprofiles` ROW_FORMAT=DYNAMIC /** CAN FAIL **/;
 ALTER TABLE `#__j2commerce_paymentprofiles`
     MODIFY `payment_token` VARCHAR(255) NOT NULL DEFAULT '';
-ALTER TABLE `#__j2commerce_paymentprofiles`
-    ADD UNIQUE KEY `uq_user_provider_env_token` (`user_id`,`provider`,`environment`,`payment_token`) /** CAN FAIL **/;
